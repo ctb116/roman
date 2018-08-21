7dc4785084c2a15906330c3a2f7cecb6a18c333b
Catherine Bradley <catherinetybradley@gmail.com>
2018-08-20 21:15:32 -0700

    add exact number to numeral converter

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
+// buisness logic
+var numbers = [1, 5, 10, 50, 100, 500, 1000];
+var numerals = ["I", "V", "X", "L", "C", "D", "M"];
+
+    console.log(numerals[1]);
+
+function convert(Num) {
+  if (num = 5) {
+    return numerals[1];
+  }
+}
+
+
+
+// user interface
 $(document).ready(function(){
   $("form#converter").submit(function(event) {
     event.preventDefault();
 
-    var userinput = $("input#num").val();
+    var userInputNum = parseInt($("input#num").val());
+    console.log(userInputNum);
 
-    console.log(userinput);
+    $("#output ul").append("<li>" + convert(userInputNum) + "</li>");
 
   });
 });
